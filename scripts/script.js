d457192c77cdfe58aa3fcf70b6c2854a8cd915ea
RolandPlawgo <rolandplawgo@gmail.com>
2024-02-04 00:01:03 +0100

    bug fix: subtitle information added more than once to some videos

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,45 @@
-let key = '';
+let apiKey = '';
+
+setInterval(addSubtitleInfo, 10000);
 
 
-setInterval(addSubtitleInfo, 1000);
-
+let videosWithInfo = [];
 function addSubtitleInfo()
 {
     let metaDataElement = document.querySelectorAll('[id=meta]');
     metaDataElement.forEach(element => {
-        if (element.querySelector('[id=metadata] .subtitle-info') == null) {
-                if (element.querySelector('[id=metadata]') != null) {                   
-                    
-                    let videoLink = element.querySelector('[id=video-title-link]');
-                    let videoId = videoLink.href.replace("https://www.youtube.com/watch?v=", "");
-                 
+        if (element.querySelector('[id=metadata]') != null) {                   
+            let videoLink = element.querySelector('[id=video-title-link]');
+            if (!videosWithInfo.includes(videoLink.title)) {
+                videosWithInfo.push(videoLink.title);
 
-                    let url = 'https://youtube.googleapis.com/youtube/v3/captions?part=snippet&videoId=' + videoId + '&key=' + key;
-                    
-                    fetch(url)
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log(data);
+                let videoId = videoLink.href.replace("https://www.youtube.com/watch?v=", "");
+                
+                let url = 'https://youtube.googleapis.com/youtube/v3/captions?part=snippet&videoId=' + videoId + '&key=' + apiKey;
 
-                            let subtitleInfoText = '';
-                            data.items.forEach(item => {
-                                console.log(item.snippet.language);
-                                subtitleInfoText += item.snippet.language + ' ';
-                                if (item.snippet.trackKind == 'asr') {
-                                    subtitleInfoText += '[a]  ';
-                                }
-                            });
-
-                            let p = document.createElement('p');
-                            p.style.color = '#de7c14';
-                            p.innerText = subtitleInfoText;
-                            p.style.fontSize = '1.4rem';
-                            console.log(p.innerText);
-                            p.classList.add('subtitle-info');
-                            element.querySelector('[id=metadata]').appendChild(p);
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        let subtitleInfoText = '';
+                        data.items.forEach(item => {
+                            console.log(item.snippet.language);
+                            subtitleInfoText += item.snippet.language;
+                            if (item.snippet.trackKind == 'asr') {
+                                subtitleInfoText += '[a] ';
+                            } 
+                            else {
+                                subtitleInfoText += ' ';
+                            }
                         });
+
+                        let p = document.createElement('p');
+                        p.style.color = '#de7c14';
+                        p.innerText = subtitleInfoText;
+                        p.style.fontSize = '1.4rem';
+                        console.log(p.innerText);
+                        p.classList.add('subtitle-info');
+                        element.querySelector('[id=metadata]').appendChild(p);
+                    });
             }
         }
     });
